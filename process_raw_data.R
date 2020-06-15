a1c75d6f35d5ad8342c4efaa2da13f2c1cc19b48
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-06-15 15:15:38 -0500

    Including code to create index page by Greenhouse

--- process_raw_data.R
+++ process_raw_data.R
@@ -5,9 +5,15 @@ clean <- function(str, from = "_", to = "-", FUN = tolower) {
 
 generate_labels <- function(inputFile, overwrite = FALSE){
   data <- read.csv(inputFile)
+  IDs <- NULL
+  COLS <- 6
+  LABEL_PATH <- ""
   for (i in 1:nrow(data)) {
     if (data[i, 1] == "" || data[i, 2] == "")
       next
+    #IDs <- paste0(IDs, "[", data[i, 2], "](", clean(data[i, 2]), ") ")
+    IDs <- rbind(IDs, paste0("[", data[i, 2], "](", clean(data[i, 2]), ")"))
+    #IDs <- rbind(IDs, "")
     new_Rmd_name <- paste0("content/label/", clean(data[i, 1]), "-", clean(data[i, 2]), ".Rmd")
     if (!file.exists(new_Rmd_name) || overwrite) {
       new_Rmd <- file(new_Rmd_name)
@@ -21,8 +27,8 @@ generate_labels <- function(inputFile, overwrite = FALSE){
           "tags:",
           paste0("  - ", clean(data[i, 2], "_\\d*", "", toupper)),
           "aliases:",
-          paste0("  - label/", clean(data[i, 1]), "-", clean(data[i, 2])),
-          paste0("  - label/", clean(data[i, 1], to = ""), "-", clean(data[i, 2], to = "")),
+          paste0("  - ", LABEL_PATH, clean(data[i, 1]), "-", clean(data[i, 2])),
+          paste0("  - ", LABEL_PATH, clean(data[i, 1], to = ""), "-", clean(data[i, 2], to = "")),
           "---",
           "",
           paste0("# **House**: ", data[i, 1]),
@@ -34,4 +40,30 @@ generate_labels <- function(inputFile, overwrite = FALSE){
     else
       print(paste0("FILE: ", new_Rmd_name, " already exists."))
   }
+  
+  for(i in 1:(COLS - nrow(IDs) %% COLS))
+    IDs <- c(IDs, "")
+  
+  links <- knitr::kable(matrix(IDs, ncol = COLS, byrow = TRUE), "html")
+  
+  new_house_Rmd_name <- paste0("content/label/", clean(data[1, 1]), ".Rmd")
+  new_house_Rmd <- file(new_house_Rmd_name)
+  writeLines(
+    c("---",
+      "author: Roberto Villegas-Diaz",
+      "tags:",
+      "  - index",
+      "title: GH118",
+      "aliases:",
+      paste0("  - ", LABEL_PATH, tolower(data[1, 1])),
+      paste0("  - ", LABEL_PATH, clean(data[1, 1], to = "")),
+      "---",
+      "```{r, echo = FALSE}",
+      "knitr::kable(matrix(c(", paste0("'",IDs,"'", collapse = ", "), 
+                           "), ncol = ", COLS, ", byrow = TRUE), 'html', booktabs = TRUE, table.attr='class=\"id_links\"')",
+      "```"
+    ),
+    new_house_Rmd_name
+  )
+  close(new_house_Rmd)
 }
